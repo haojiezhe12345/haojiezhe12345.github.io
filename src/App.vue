f915f687d584328b049d56f850a9b25badf4912b
浩劫者12345 <3112611479@qq.com>
2025-06-19 15:22:11 +0800

    Revert "add server maintenance notice"
    This reverts commit 8935a9a4b5bf6c0ee2717965ef4427205f3e5daf.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import FloatMsgs from './components/FloatMsgs';
-
 const str = 'Vue 3 loaded'
 console.log(str)
-
-FloatMsgs.show({ type: 'warn', msg: `服务器维护中, 预计1小时内恢复<br>预计恢复时间: ${new Date('2025-06-19T15:30:00+08:00').toLocaleString()}`, persist: true })
 
 </script>
 
